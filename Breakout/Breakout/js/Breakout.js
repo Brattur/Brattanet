3653a0058d63e3da473e53f3e90f23ad2a81c54a
1710992509 <kristjan.eli@gmail.com>
2016-12-04 12:14:18 +0000

    Bæti við Breakout verkefni

---
+++ Breakout/Breakout/js/Breakout.js
@@ -0,0 +1,12 @@
+﻿var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.arc(240, 160, 20, 0, 2*Math.PI);
+ctx.fillStyle = 'blue';
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50);
+
