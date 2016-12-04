c4a4ca4c18cd715e64d1bcfb77a208b16c0a6af0
1710992509 <kristjan.eli@gmail.com>
2016-12-04 12:36:35 +0000

    Breakout Function

--- Breakout/Breakout/js/Breakout.js
+++ Breakout/Breakout/js/Breakout.js
@@ -1,12 +1,11 @@
 ﻿var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, 2*Math.PI);
-ctx.fillStyle = 'blue';
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-
+function draw() {
+    ctx.beginPath();
+    ctx.arc(240, 160, 20, 0, 2 * Math.PI);
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+}
+setInterval(draw, 10);
