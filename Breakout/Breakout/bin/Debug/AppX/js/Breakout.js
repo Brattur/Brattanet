9c3cee63d3435c1de45a03ee7f7dc06a62652933
1710992509 <kristjan.eli@gmail.com>
2016-12-04 23:51:13 +0000

    Lagfæringar

--- Breakout/Breakout/bin/Debug/AppX/js/Breakout.js
+++
@@ -1,8 +0,0 @@
-﻿var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d");
-
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, 2*Math.PI);
-ctx.fillStyle = "FFFF00";
-ctx.fill();
-ctx.closePath();
