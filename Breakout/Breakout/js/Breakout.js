b9bb6ee4a8fc1337730b4c181dae6089955de705
1710992509 <kristjan.eli@gmail.com>
2016-12-04 15:32:13 +0000

    Bæta við stöng og game over

--- Breakout/Breakout/js/Breakout.js
+++ Breakout/Breakout/js/Breakout.js
@@ -1,11 +1,97 @@
 ﻿var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
 
-function draw() {
+function drawBall()
+{
     ctx.beginPath();
-    ctx.arc(240, 160, 20, 0, 2 * Math.PI);
-    ctx.fillStyle = 'blue';
+    ctx.arc(x, y, ballRadius, 0, 2 * Math.PI);
+    ctx.fillStyle = '#0095DD';
     ctx.fill();
     ctx.closePath();
+
 }
+
+function drawPaddle()
+{
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+
+}
+
+function draw()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    drawPaddle();
+    x += dx;
+    y += dy;
+
+    if (y + dy < ballRadius)
+    {
+        dy = -dy;
+    }
+
+    else if (y + dy > canvas.height - ballRadius)
+    {
+        alert("GAME OVER");
+        document.location.reload();
+    }
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius)
+    {
+        dx = -dx;
+    }
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth)
+    {
+        paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0)
+    {
+        paddleX -= 7;
+    }
+}
+
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e)
+{
+    if (e.keyCode == 39)
+    {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37)
+    {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e)
+{
+    if (e.keyCode == 39)
+    {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37)
+    {
+        leftPressed = false;
+    }
+}
+
 setInterval(draw, 10);
